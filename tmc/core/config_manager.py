4341cbf45ce5b3b1c93d247822e2009140546c47
Tyler Wright <tylwright@gmail.com>
2018-08-26 20:50:54 -0400

    Dynamic config viewer working

--- tmc/core/config_manager.py
+++ tmc/core/config_manager.py
@@ -16,3 +16,30 @@ def read_netatalk_afp_conf(method):
         c = ConfigParser()
         c.read('/etc/netatalk/afp.conf')
         return c.sections()
+
+
+def read_netatalk_afpd_conf():
+    """
+    Return the contents of /etc/netatalk/afpd.conf
+    :return: /etc/netatalk/afpd.conf configuration file
+    """
+    with open('/etc/netatalk/afpd.conf') as f:
+        return "<br/>".join(f.read().split("\n"))
+
+
+def read_netatalk_volumes_conf():
+    """
+    Return the contents of /etc/netatalk/AppleVolumes.default
+    :return: /etc/netatalk/AppleVolumes.default configuration file
+    """
+    with open('/etc/netatalk/AppleVolumes.default') as f:
+        return "<br/>".join(f.read().split("\n"))
+
+
+def read_avahi_daemon_conf():
+    """
+    Return the contents of /etc/avahi/avahi-daemon.conf
+    :return: /etc/avahi/avahi-daemon.conf configuration file
+    """
+    with open('/etc/avahi/avahi-daemon.conf') as f:
+        return "<br/>".join(f.read().split("\n"))
--- tmc/core/templatetags/core_filters.py
+++ tmc/core/templatetags/core_filters.py
@@ -1,5 +1,6 @@
 from django import template
-from core.config_manager import read_netatalk_afp_conf
+from core.config_manager import read_netatalk_afp_conf, read_netatalk_afpd_conf, read_netatalk_volumes_conf, \
+    read_avahi_daemon_conf
 
 
 register = template.Library()
@@ -13,3 +14,33 @@ def read_netatalk_afp_config(str):
     :return: Contents of /etc/netatalk/afp.conf
     """
     return read_netatalk_afp_conf(method='raw')
+
+
+@register.filter(name='read_netatalk_afpd_config')
+def read_netatalk_afpd_config(str):
+    """
+    Returns contents of /etc/netatalk/afpd.conf
+    :param str: None (given only to use filter in this way)
+    :return: Contents of /etc/netatalk/afpd.conf
+    """
+    return read_netatalk_afpd_conf()
+
+
+@register.filter(name='read_netatalk_volumes_config')
+def read_netatalk_volumes_config(str):
+    """
+    Returns contents of /etc/netatalk/AppleVolumes.default
+    :param str: None (given only to use filter in this way)
+    :return: Contents of /etc/netatalk/AppleVolumes.default
+    """
+    return read_netatalk_volumes_conf()
+
+
+@register.filter(name='read_avahi_daemon_config')
+def read_avahi_daemon_config(str):
+    """
+    Returns contents of /etc/avahi/avahi-daemon.conf
+    :param str: None (given only to use filter in this way)
+    :return: Contents of /etc/avahi/avahi-daemon.conf
+    """
+    return read_avahi_daemon_conf()
